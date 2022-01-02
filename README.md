58c229df88a90b780021ff5066c91d58c5e447e7
Anthony Herrera <anthonyherrera24@gmail.com>
2022-01-02 16:45:15 -0600

    initial script to retrieve data is working

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# python_hubitat_logging
-Logging Hubitat Data with Python
+# Python Hubitat Data Logging
+
+There exists other logging options out there that utilize something call "node-red" to capture and store data in
+InfluxDB. While interesting, I couldn't really do much with it because I know nothing about Node programming. So, I took
+the general format and layout and converted it all to Python. 
+
+The general data flow goes something like this: `hubitat => python => influx` and then you can use graphana on top of
+that to view the data.
---
+++ main/main.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# coding: utf-8
+"""
+    Simple script that listens for Hubitat data on the event websocket and then adds that data to an influxdb database.
+    Note the env file should have "hubitat_ip", "influx_ip", "username", and "password".
+"""
+import json
+from influxdb import InfluxDBClient
+from time import time
+import websocket
+
+
+
+env_file = '.env'
+with open(env_file) as f:
+    env_data = json.load(f)
+    
+HUB_ADDR = env_data["hubitat_ip"]
+INFLUX_ADDR = env_data["influx_ip"]
+USER = env_data["username"]
+PASS = env_data["password"]
+
+e_sock = f"ws://{HUB_ADDR}/eventsocket"
+
+
+
+def message_parser(message):
+    """
+    Exract info required for data insertion
+    """
+
+    # Save data for later
+    filename = "loggy.mclogface"
+    with open(filename, "a") as f:
+            f.write(json.dumps(message) + ",\n")
+
+    # Used to remap values if needed
+    value_mapping = {
+        "on": 1,
+        "off": 0
+    }
+
+    measurement=message["name"]
+    displayName=message["displayName"]
+    value=message["value"]
+    unit=message["unit"]
+    did=message["deviceId"]
+    timestamp=message["ts"]
+    if value in value_mapping.keys():
+        value = value_mapping[value]
+
+    try:
+        value=float(value)
+    except ValueError as e:
+        # YES I'm passing this - this is because the incoming value is actually a string, let it free
+        pass
+
+    if type(value) == str:
+        line = f'{measurement},deviceId={did} value="{value}",unit="{unit}",displayName="{displayName}" {int(timestamp*1e9)}'
+    else:
+        line = f'{measurement},deviceId={did} value={value},unit="{unit}",displayName="{displayName}" {int(timestamp*1e9)}'
+    return line
+
+
+
+def insert_message(wsapp, message):
+    """
+    Write the message to the database after parsing
+    """
+    message = json.loads(message)
+    message["ts"] = time()
+    line = message_parser(message)
+    client =InfluxDBClient(
+        host=INFLUX_ADDR,
+        port=8086,
+        username=USER,
+        password=PASS)
+
+    client.write([line], {'db': 'hubitat'}, 204, 'line')
+    client.close()
+
+wsapp = websocket.WebSocketApp(e_sock, on_message=insert_message)
+wsapp.run_forever()
